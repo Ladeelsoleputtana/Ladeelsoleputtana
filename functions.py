deb2ca7ca0bad8bbb32832555851b49ac13bff26
Arnaque_du_Tsar <edouard.dion.2004@gmail.com>
2023-11-29 17:14:55 +0100

    ma

--- functions.py
+++ functions.py
@@ -1,3 +1,17 @@
+import os
+
+def list_of_files(directory, extension):
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
+
+# Call of the function
+directory = "./speeches"
+files_names = list_of_files(directory, "txt")
+print_list(files_names)
+
 def speeches(a,b,c,d,e,f,g,h):
     a = "Nomination_Chirac1.txt"
     b = "Nomination_Chirac2.txt"
@@ -13,17 +27,3 @@ def speeches(a,b,c,d,e,f,g,h):
         x = int(input("which speech would you like to read ?"))
         if x == 1:
             with open("Nomination_Chirac1.txt")
-
-import os
-
-def list_of_files(directory, extension):
-    files_names = []
-    for filename in os.listdir(directory):
-        if filename.endswith(extension):
-            files_names.append(filename)
-    return files_names
-
-# Call of the function
-directory = "./speeches"
-files_names = list_of_files(directory, "txt")
-print_list(files_names)
