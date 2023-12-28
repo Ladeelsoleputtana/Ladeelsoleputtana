c5b883da6db7ebeabecd916f75bfe4f95789260b
Hélène <lihelene22092@gmail.com>
2023-12-28 22:34:41 +0100

    menu

--- main.py
+++ main.py
@@ -7,9 +7,28 @@ part1.IDF()
 part1.TF_IDF()
 part1.TF_IDF_Chaque_Doc()
 matrice = part1.matrice_tf_idf()
-print(matrice)
-part1.non_important()
-part1.score_haut()
-part1.repetition()
-part1.Nation()
-part1.climat_eco()
+
+boom = 1
+while boom == 1:
+    print("Si vous voulez afficher la matrice TF-IDF, tapez 1")
+    print("Si vous voulez afficher la liste des mots les moins importants dans le corpus de documents, tapez 2")
+    print("Si vous voulez afficher le mot ayant le score TF-IDF le plus élevé, tapez 3")
+    print("Si vous voulez afficher le mot le plus répéter par le président Chirac, tapez 4")
+    print("Si vous voulez afficher les présidents qui ont parlé de la Nation et ceux qui ont le plus répéter, tapez 5")
+    print("Si vous voulez afficher le président ayant parler du climat et de l'écologie, tapez 6")
+    print("Si vous voulez arrêter le programme, tapez 7")
+    Choix_menu = int(input("Choisissez un menu : "))
+    if Choix_menu == 1:
+        print(matrice)
+    elif Choix_menu == 2:
+        part1.non_important()
+    elif Choix_menu == 3:
+        part1.score_haut()
+    elif Choix_menu == 4:
+        part1.repetition()
+    elif Choix_menu == 5:
+        part1.Nation()
+    elif Choix_menu == 6:
+        part1.climat_eco()
+    elif Choix_menu == 7:
+        boom = 0
